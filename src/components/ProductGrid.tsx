67b2847727cc17fe7ddb8eba8f1b58ee6cf495b8
Kamran Davar <kamran.davar.KD@GMAIL.COM>
2023-12-11 16:40:32 +0330

    Fetch the games.

---
+++ src/components/ProductGrid.tsx
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react"
+import apiClient from "../services/api-client";
+import { Text } from "@chakra-ui/react";
+
+interface Product {
+    id: number;
+    title: string;
+}
+
+
+export const ProductGrid = () => {
+const [products, setProducts] = useState<Product[]>([]);
+const [error, setError] = useState('');
+
+useEffect(() => {
+    apiClient.get<Product[]>('/products')
+    .then( res => setProducts(res.data))
+    .catch(err => setError(err.message))
+})
+
+  return (
+    <>
+    {error && <Text>{error}</Text>}
+    <ul>
+        {products.map( product => <li key={product.id}>{product.title}</li>)}
+    </ul>
+    </>
+  )
+}
