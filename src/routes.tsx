9a2351ee8a9118380e6127012ff799d1b7c926a0
Kamran Davar <kamran.davar.KD@GMAIL.COM>
2024-01-08 16:07:33 +0330

    Handle errors

--- src/routes.tsx
+++ src/routes.tsx
@@ -12,7 +12,7 @@ const router = createBrowserRouter([
         children: [
             {index: true, element: <HomePage /> },
             {path: 'products/:id', element: <ProductDetailPage />}
-        ]
+        ],
     }
 ])
 
